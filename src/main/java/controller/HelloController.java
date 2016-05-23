75f95ae1da0233a285fe7db6a6e531d454d62fa3
ypfyhs <562394562@qq.com>
2016-05-24 07:03:22 +0800

    添加了一个注册的示例代码，主要在UserService里面的userstest方法里面，各步骤的注释都已经写好了，然后为了测试在HelloController里面写了一个main函数执行这个方法，感兴趣可以去https://leancloud.cn/data.html?appid=itMaof6Ex5Qd1Ia2bevnoNx3-gzGzoHsz#/_User 把这条数据删了然后跑跑看，因为毕设的事情拖了好久才做了这个，抱歉

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import service.UserService;
 
 @Controller
 @RequestMapping("/")
@@ -12,5 +13,16 @@ public class HelloController {
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
 		return "hello";
+	}
+
+	public static void main(String args[])
+	{
+		UserService userService = new UserService();
+		try{
+			userService.usersTest();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
 	}
 }
